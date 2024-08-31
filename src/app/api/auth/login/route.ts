24bc4ad2bf2606e73db42b87d5b020dbd896dcb1
sathyan4321 <sathyanr.4321@gmail.com>
2024-08-31 12:29:33 +0530

    1

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -4,16 +4,15 @@ import { signIn } from '@/auth';
 import { DEFAULT_LOGIN_REDIRECT_URL } from "@/lib/routes";
 import { AuthError } from "next-auth";
 
-
-export async function POST(request: Request) {
+export async function POST(request: Request): Promise<NextResponse | Response> {
     const body = await request.json();
     const { email, password } = body;
     
-    console.log("LOGIN API: ",email, password);
+    console.log("LOGIN API: ", email, password);
 
     const existingUser = await getUserByEmail(email);
 
-    if(!existingUser){
+    if (!existingUser) {
         return new NextResponse("User does not exist", { status: 400 });
     }
 
@@ -24,17 +23,19 @@ export async function POST(request: Request) {
             redirectTo: DEFAULT_LOGIN_REDIRECT_URL,
             redirect: true,
         });
+        return NextResponse.redirect(DEFAULT_LOGIN_REDIRECT_URL);
     } catch (error) {
         if (error instanceof AuthError) {
             switch (error.type) {
                 case 'CredentialsSignin':
                     return new NextResponse('Invalid Credentials!', { status: 400 });
                 case 'CallbackRouteError':
-                    return { error: error.cause?.err?.message };
+                    return new NextResponse(error.cause?.err?.message || 'Callback route error', { status: 400 });
                 default:
                     return new NextResponse('Something went wrong!', { status: 500 });
             }
+        } else {
+            return new NextResponse('Unexpected error occurred!', { status: 500 });
         }
     }
-
 }
